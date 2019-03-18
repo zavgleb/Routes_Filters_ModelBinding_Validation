c63ba757fed6706d65187c2d7d8bfedee04a3d5b
Manish Agrawal <manish.agrawal@gmail.com>
2019-03-18 12:36:40 -0400

    Add project files.

---
+++ Routes_Filters_ModelBinding_Validation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Routes_Filters_ModelBinding_Validation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
